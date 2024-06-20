711f97b369b9a9634c98f75efe5411e3ee27c8ac
Angel Gasca <angel.cesar@onexo.mx>
2024-06-20 11:33:30 -0600

    feat: home page

--- src/pages/home-page-styles.js
+++ src/pages/home-page-styles.js
@@ -4,4 +4,26 @@ export default css`
   h1 {
     font-size: 1.2rem;
   }
+  :host{
+    font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 1rem;
+    justify-content: center;
+    align-items: center;
+    padding: 2rem 1rem;
+  }
+  .btn__favorites{
+    position:fixed;
+    right: 1rem;
+    bottom:1rem;
+    background-color: hsla(0, 0%, 100%, 0.918);
+    border: 1px solid hsla(0, 0%, 0%, 0.588);
+    border-radius: 50%;
+    width: 5rem;
+    height: 5rem;
+  }
+  .btn__favorites p{
+    margin: 0;
+  }
 `
--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -1,5 +1,7 @@
 import { LitElement, html } from "lit-element";
 import styles from "./home-page-styles"
+import "../components/character-card/character-card";
+import "../components/favorite-characters/favorite-characters";
 
 class HomePage extends LitElement {
   static get styles() {
@@ -7,16 +9,76 @@ class HomePage extends LitElement {
   }
 
   static get properties() {
-    return {}
+    return {
+      characters: {
+        type: Array
+      },
+      favorities: {
+        type: Array
+      },
+      showDialog:{
+        type: Boolean
+      }
+    }
   }
 
   constructor() {
     super()
+    this.characters = []
+    this.addEventListener("update-favorite-character", this.handleUpdateFavorites)
+    this.addEventListener("update-close-dialog", this.closeDialog)
+    this.favorities = []
+    this.showDialog = false
+  }
+  connectedCallback() {
+    super.connectedCallback()
+    this.getCharacters()
+  }
+  handleUpdateFavorites(event) {
+    const { character } = event.detail
+    console.log();
+    if (!this.favorities.includes(character)) {
+      this.favorities.push(character)
+    }
+    else {
+      const positionCharacter = this.favorities.indexOf(character)
+      this.favorities.splice(positionCharacter, 1)
+    }
+    console.log("update characteres");
+    console.log(this.favorities);
+    this.requestUpdate();
+  }
+  dialog(){
+    this.showDialog = true
+    this.requestUpdate();
+  }
+  closeDialog(event){
+    const { showDialog } = event.detail
+    this.showDialog = showDialog;
+    this.requestUpdate();
+  }
+
+  async getCharacters() {
+    await fetch("https://rickandmortyapi.com/api/character/?page=1")
+      .then((res) => (res.json()))
+      .then(({ info, results }) => this.characters = results)
   }
 
   render() {
     return html`
-      <h1>Home page</h1>
+      <button class="btn__favorites" @click="${this.dialog}">
+        <p>Ver</p>
+        <p>favoritos</p>
+      </button>
+      <favorite-characters .showDialog="${this.showDialog}" .characters="${this.favorities}"></favorite-characters>
+
+      ${this.characters.map((character) => (
+      html`<character-card 
+        .character="${character}" 
+        .isFavorite="${this.favorities.includes(character)}"
+        />
+        `
+    ))}
     `
   }
 }
